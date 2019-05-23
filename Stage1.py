f14527d4e25ab89349193bae61f0490706d20787
Jack Gartland <jack@DN0a22612b.SUNet>
2019-05-22 22:57:02 -0700

    added the simulate function

--- Stage1.py
+++ Stage1.py
@@ -88,6 +88,50 @@ class QLearningAlgorithm:
             self.weights[f] -= v
         # END_YOUR_CODE
 
-game = Grid()
-state = State(game)
-new_state = state.generateSuccessor((0,0))
+def simulate(mdp, rl, numTrials=10, maxIterations=1000, verbose=False,
+             sort=False):
+    # Return i in [0, ..., len(probs)-1] with probability probs[i].
+    def sample(probs):
+        target = random.random()
+        accum = 0
+        for i, prob in enumerate(probs):
+            accum += prob
+            if accum >= target: return i
+        raise Exception("Invalid probs: %s" % probs)
+
+    totalRewards = []  # The rewards we get on each trial
+    for trial in range(numTrials):
+        state = mdp.startState()
+        sequence = [state]
+        totalDiscount = 1
+        totalReward = 0
+        for _ in range(maxIterations):
+            action = rl.getAction(state)
+            transitions = mdp.succAndProbReward(state, action)
+            if sort: transitions = sorted(transitions)
+            if len(transitions) == 0:
+                rl.incorporateFeedback(state, action, 0, None)
+                break
+
+            # Choose a random transition
+            i = sample([prob for newState, prob, reward in transitions])
+            newState, prob, reward = transitions[i]
+            sequence.append(action)
+            sequence.append(reward)
+            sequence.append(newState)
+
+            rl.incorporateFeedback(state, action, reward, newState)
+            totalReward += totalDiscount * reward
+            totalDiscount *= mdp.discount()
+            state = newState
+        if verbose:
+            print "Trial %d (totalReward = %s): %s" % (trial, totalReward, sequence)
+        totalRewards.append(totalReward)
+    return totalRewards
+
+
+mdp = MDP()
+mdp.computeStates()
+rl = QLearningAlgorithm(0.95, 0.2)
+# We call this here so that the stepSize will be 1
+simulate(mdp, rl, 30000)
